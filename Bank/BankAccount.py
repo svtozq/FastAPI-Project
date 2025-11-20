1f263dcc15d6ebf790511316c2f7006ba2ef2076
strauss <aff.strauss@gmail.com>
2025-11-20 15:44:01 +0100

    Modify bank create & close acc function

--- Bank/BankAccount.py
+++ Bank/BankAccount.py
@@ -1,36 +1,59 @@
+import datetime
 import random
 from fastapi import FastAPI, Depends, HTTPException, APIRouter
 from sqlalchemy.orm import Session
 from DB.database import engine, get_db
 from DB import models
 from Users.signIn import get_user
+from pydantic import BaseModel
 
 
 router = APIRouter(prefix="/Bank", tags=["BankAccount"])
+
+class BankAccount(BaseModel):
+    type: str
 
 
 "Partie de Lenny pour les comptes bancaires"
 # ✅ POST - Créer un compte bancaire
 @router.post("/accounts/")
-def create_account(user=Depends(get_user), db: Session = Depends(get_db)):
+def create_account(data : BankAccount, user=Depends(get_user), db: Session = Depends(get_db)):
+    # Génère un IBAN fictif
+    iban = "FR" + str(random.randint(10, 99)) + str(random.randrange(10 ** 11, 10 ** 12))
+
+    now = datetime.datetime.now()
+
+    acc_type = data.type
 
     # Vérifie s’il a déjà un compte actif
     account_count = db.query(models.BankAccount).filter(
-        models.BankAccount.user_id == user["user_id"]
+        models.BankAccount.user_id == user["user_id"],
+        models.BankAccount.clotured == False
     ).count()
 
     if account_count >= 5:
         raise HTTPException(status_code=400, detail="L'utilisateur a atteint la limite de 5 comptes maximum")
 
-    # Génère un IBAN fictif
-    iban = "FR" + str(random.randint(10, 99)) + str(random.randrange(10 ** 11, 10 ** 12))
+    if account_count < 1 :
+        account = models.BankAccount(
+            user_id=user["user_id"],
+            iban=iban,
+            balance=0,
+            clotured=False,
+            type="Compte Principal",
+            BankAccount_date=now,
+        )
 
-    account = models.BankAccount(
-        user_id=user["user_id"],
-        iban=iban,
-        balance=0,
-        clotured=False,
-    )
+    elif account_count >= 1 :
+        account = models.BankAccount(
+            user_id=user["user_id"],
+            iban=iban,
+            balance=0,
+            clotured=False,
+            type=acc_type,
+            BankAccount_date=now,
+        )
+
     db.add(account)
     db.commit()
     db.refresh(account)
@@ -70,13 +93,26 @@ def get_user_accounts(user=Depends(get_user), db: Session = Depends(get_db)):
 
 # ✅ PUT - Clôturer un compte
 @router.put("/accounts/{account_id}/close")
-def close_account(account_id: int, db: Session = Depends(get_db)):
-    account = db.query(models.BankAccount).filter(models.BankAccount.id == account_id).first()
+def close_account(account_id: int, user=Depends(get_user), db: Session = Depends(get_db)):
+    main_account = db.query(models.BankAccount).filter(
+        models.BankAccount.user_id == user["user_id"],
+        models.BankAccount.type == "Compte Principal"
+    ).first
+
+    account = db.query(models.BankAccount).filter(
+        models.BankAccount.id == account_id,
+                models.BankAccount.user_id == user["user_id"]
+    ).first()
+
+
     if not account:
         raise HTTPException(status_code=404, detail="Compte introuvable")
 
     if account.clotured:
         raise HTTPException(status_code=400, detail="Compte déjà clôturé")
+
+    if account.type == "Compte Principal":
+        raise HTTPException(status_code=400, detail="Impossible de clôturer le compte principal")
 
     account.clotured = True
     db.commit()
